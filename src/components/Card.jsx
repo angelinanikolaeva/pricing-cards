c7844ba3429431d1243e753f28b7dec5dfc8f6b7
Angelina Nikolaeva <77636381+angelinanikolaeva@users.noreply.github.com>
2021-11-02 09:38:04 +0300

    added minor changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 function Card(props) {
   const { nameRate, priceRate, numberMb } = props;
   const [selected, setSelected] = useState(false);
